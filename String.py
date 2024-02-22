169e807f96379ffbeff6690499d227ef4cfb5348
Anusree-at <anuat2017@gmail.com>
2024-02-22 23:42:56 +0530

    string

---
+++ String.py
@@ -0,0 +1,25 @@
+string = 'Hello world'
+print(len(string))
+
+print('world' in string)
+print(string.replace('world','python'))
+
+string = 'Hello'
+string2 = 'World'
+print(string + ' ' + string2)
+
+word = 'beautiful'
+print(word[0:8])
+
+a = "world"
+print(a[-2:] + a[-2:] + a[-2:] + a[-2:])
+
+st = 'hello'
+st2 = ('m'+st[1:-1]+'z')
+print(st2)
+
+word2 = input('enter a word')
+print(word2.lower())
+print(word2.upper())
+
+
